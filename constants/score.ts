a1cb661b1efb099695b1f8888e048d165a0ff8ef
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-06 21:27:28 +0900

    feat: 定数作成

---
+++ constants/score.ts
@@ -0,0 +1,40 @@
+export const Tap = {
+  criticalPerfect: 500,
+  perfect: 500,
+  great: 500 * 0.8,
+  good: 500 * 0.5,
+  miss: 0,
+} as const;
+
+export const Hold = {
+  criticalPerfect: 1000,
+  perfect: 1000,
+  great: 1000 * 0.8,
+  good: 1000 * 0.5,
+  miss: 0,
+} as const;
+
+export const Slide = {
+  criticalPerfect: 1500,
+  perfect: 1500,
+  great: 1500 * 0.8,
+  good: 1500 * 0.5,
+  miss: 0,
+} as const;
+
+export const Touch = {
+  criticalPerfect: 500,
+  perfect: 500,
+  great: 500 * 0.8,
+  good: 500 * 0.5,
+  miss: 0,
+} as const;
+
+export const Break = {
+  criticalPerfect: 2600,
+  HighPerfect: 2550,
+  rowPerfect: 2500,
+  great: 2000,
+  good: 1000,
+  miss: 0,
+} as const;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Header } from '../components/Header';
 const Home = () => {
   return (
     <div>
-      <Header></Header>
+      <Header>maimaiでらっくす　スコアしみゅれーたー</Header>
     </div>
   );
 };
