8b31bdac042db1fbfc982fd444ccac2622cc76e9
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-11 19:51:37 +0900

    feat: でらっくスコア用のファイル作成

---
+++ constants/DXScore.ts
@@ -0,0 +1,9 @@
+export const DXScore = {
+  criticalPerfect: 3,
+  highPerfect: 2,
+  lowPerfect: 2,
+  perfect: 2,
+  great: 1,
+  good: 0,
+  miss: 0,
+} as const;
---
+++ functions/DXScoreCalc.ts
@@ -0,0 +1,52 @@
+import { Tap, Break } from '../constants/classicScore';
+import { DXScore } from '../constants/DXScore';
+
+interface ScorePointDetail {
+  evaluation: keyof typeof Tap & keyof typeof Break; //判定名
+  sum: number; //その判定を獲得したノーツ数
+}
+
+interface ScorePoint {
+  notesName: string; //ノーツ名
+  total: number; //そのノーツの総数
+  detail: ScorePointDetail[]; //判定の獲得数詳細
+}
+
+//全ノーツの最高点の合計
+export const maxScoreCalc = (scorePoint: ScorePoint) => {
+  return DXScore['criticalPerfect'] * scorePoint.total;
+};
+
+//該当ノーツの各判定ごとの合計点
+export const detailScoreCalc = (scoreDetail: ScorePointDetail) => {
+  return DXScore[scoreDetail.evaluation] * scoreDetail.sum;
+};
+
+//各ノーツの合計獲得点
+export const totalScoreCalc = (scorePoint: ScorePoint) => {
+  return scorePoint.detail.reduce(
+    (previousScore: number, currentDetail: ScorePointDetail) =>
+      previousScore + detailScoreCalc(currentDetail),
+    0
+  );
+};
+
+export const DXScoreCalc = (scorePoints: ScorePoint[]) => {
+  let maxScore = scorePoints.reduce(
+    (previousPoint: number, currentPoint: ScorePoint) => previousPoint + maxScoreCalc(currentPoint),
+    0
+  );
+
+  let totalScore = scorePoints.reduce(
+    (previousPoint: number, currentPoint: ScorePoint) =>
+      previousPoint + totalScoreCalc(currentPoint),
+    0
+  );
+  if (maxScore === 0) {
+    return 0;
+  }
+  return {
+    max: maxScore,
+    total: totalScore,
+  };
+};
