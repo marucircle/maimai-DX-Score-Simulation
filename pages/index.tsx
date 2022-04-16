bbc6733250e8163dedc52803d28384542f638e53
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-16 23:06:36 +0900

    feat: メインページにTable追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from '../components/Header';
+import { Table } from '../components/Table';
 
 const Home = () => {
   return (
     <div>
       <Header>maimaiでらっくす　スコアしみゅれーたー</Header>
+      <Table></Table>
     </div>
   );
 };
