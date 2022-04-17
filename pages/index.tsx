3473b3d248e7a20b1806ea32242a46c7bf578d00
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-17 21:17:03 +0900

    feat: メインページに新筐体のスコアボード追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ClassicScoreBoard } from '../components/ClassicScoreBoard';
+import { NewScoreBoard } from '../components/NewScoreBoard';
 import { Header } from '../components/Header';
 import { Table } from '../components/Table';
 import { useScore } from '../hooks/useScore';
@@ -9,15 +10,19 @@ const TableWrapper = styled.div`
   width: 100%;
 `;
 
-const ScoreBoards = styled.div``;
+const ScoreBoards = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Home = () => {
-  const { scoreData, editScoreData, classicScore } = useScore();
+  const { scoreData, editScoreData, classicScore, newScore } = useScore();
   return (
     <div>
       <Header>maimaiでらっくす　すこあしみゅれーたー</Header>
       <ScoreBoards>
         <ClassicScoreBoard scoreData={classicScore} />
+        <NewScoreBoard scoreData={newScore} />
       </ScoreBoards>
       <TableWrapper>
         <Table scoreData={scoreData} onChange={editScoreData} />
