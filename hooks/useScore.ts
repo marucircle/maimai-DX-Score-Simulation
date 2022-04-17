a827a4a375fc614df6001fe42b5e950a41a46040
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-17 20:21:22 +0900

    feat: スコア変更時に計算を実行するように処理を実装

--- hooks/useScore.ts
+++ hooks/useScore.ts
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { data } from 'msw/lib/types/context';
+import { useEffect, useState } from 'react';
+import { classicScoreCalc } from '../functions/classicScoreCalc';
 import { Evaluation, NotesType, ScoreDataType } from '../types/score';
 
 export const useScore = () => {
-  const [classicScore, setClassicScore] = useState({ max: 0, total: 0 });
+  const [classicScore, setClassicScore] = useState({
+    max: 0,
+    total: 0,
+    maxBreakBonus: 0,
+    totalBreakBonus: 0,
+  });
   const [newScore, setNewScore] = useState({
     max: 0,
     total: 0,
@@ -15,8 +22,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
-        highPerfect: -1,
-        lowPerfect: -1,
+        highPerfect: 0,
+        lowPerfect: 0,
         perfect: 0,
         great: 0,
         good: 0,
@@ -27,8 +34,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
-        highPerfect: -1,
-        lowPerfect: -1,
+        highPerfect: 0,
+        lowPerfect: 0,
         perfect: 0,
         great: 0,
         good: 0,
@@ -39,8 +46,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
-        highPerfect: -1,
-        lowPerfect: -1,
+        highPerfect: 0,
+        lowPerfect: 0,
         perfect: 0,
         great: 0,
         good: 0,
@@ -51,8 +58,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
-        highPerfect: -1,
-        lowPerfect: -1,
+        highPerfect: 0,
+        lowPerfect: 0,
         perfect: 0,
         great: 0,
         good: 0,
@@ -65,7 +72,7 @@ export const useScore = () => {
         criticalPerfect: 0,
         highPerfect: 0,
         lowPerfect: 0,
-        perfect: -1,
+        perfect: 0,
         great: 0,
         good: 0,
         miss: 0,
@@ -85,6 +92,19 @@ export const useScore = () => {
         return Object.assign({}, updateData); //あんまり良くないらしい　次回以降気を付けよう
       });
   };
+
+  useEffect(() => {
+    let calcData = Object.entries(scoreData).map(([key, value]) => {
+      return {
+        notesName: key,
+        total: value.total,
+        detail: Object.entries(value.detail).map(([key, value]) => {
+          return { evaluation: key, sum: value };
+        }),
+      };
+    });
+    setClassicScore(Object.assign({}, classicScoreCalc(calcData)));
+  }, [editScoreData]);
 
   return {
     classicScore,
