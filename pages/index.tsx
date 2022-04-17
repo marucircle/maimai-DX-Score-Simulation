a29e83b157da655a0cf8dba5c94f14073021ffb6
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-17 15:31:45 +0900

    feat: indexページのTableのラッパーを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Header } from '../components/Header';
 import { Table } from '../components/Table';
+import { useScore } from '../hooks/useScore';
+
+const TableWrapper = styled.div`
+  width: 100%;
+`;
 
 const Home = () => {
+  const { scoreData, editScoreData } = useScore();
   return (
     <div>
-      <Header>maimaiでらっくす　スコアしみゅれーたー</Header>
-      <Table></Table>
+      <Header>maimaiでらっくす　すこあしみゅれーたー</Header>
+      <TableWrapper>
+        <Table scoreData={scoreData} onChange={editScoreData} />
+      </TableWrapper>
     </div>
   );
 };
