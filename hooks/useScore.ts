23d2a68998dd092263b3dd2b0809989446cee0cf
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-17 15:30:59 +0900

    feat: useScoreからScoreDataの修正機能を追加

--- hooks/useScore.ts
+++ hooks/useScore.ts
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Evaluation, NotesType, ScoreDataType } from '../types/score';
 
 export const useScore = () => {
   const [classicScore, setClassicScore] = useState({ max: 0, total: 0 });
@@ -8,12 +9,14 @@ export const useScore = () => {
     maxBreakBonus: 0,
     totalBreakBonus: 0,
   });
-  const [DXScore] = useState({ max: 0, total: 0 });
-  const [scoreData, setScoreData] = useState({
+  const [DXScore, setDXScore] = useState({ max: 0, total: 0 });
+  const [scoreData, setScoreData] = useState<ScoreDataType>({
     tap: {
       total: 0,
       detail: {
         criticalPerfect: 0,
+        highPerfect: -1,
+        lowPerfect: -1,
         perfect: 0,
         great: 0,
         good: 0,
@@ -24,6 +27,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
+        highPerfect: -1,
+        lowPerfect: -1,
         perfect: 0,
         great: 0,
         good: 0,
@@ -34,6 +39,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
+        highPerfect: -1,
+        lowPerfect: -1,
         perfect: 0,
         great: 0,
         good: 0,
@@ -44,6 +51,8 @@ export const useScore = () => {
       total: 0,
       detail: {
         criticalPerfect: 0,
+        highPerfect: -1,
+        lowPerfect: -1,
         perfect: 0,
         great: 0,
         good: 0,
@@ -56,10 +65,36 @@ export const useScore = () => {
         criticalPerfect: 0,
         highPerfect: 0,
         lowPerfect: 0,
+        perfect: -1,
         great: 0,
         good: 0,
         miss: 0,
       },
     },
   });
+
+  const editScoreData = (value: string, notesType: NotesType, evaluation: Evaluation) => {
+    const re = /^[0-9\b]+$/;
+    if (re.test(value) && Number(value) <= 100000 && Number(value) >= 0)
+      setScoreData((updateData) => {
+        if (updateData[notesType].detail[evaluation] === -1) return updateData;
+
+        updateData[notesType].total += -updateData[notesType].detail[evaluation] + Number(value);
+        updateData[notesType].detail[evaluation] = Number(value);
+        // console.log(updateData);
+        return Object.assign({}, updateData); //あんまり良くないらしい　次回以降気を付けよう
+      });
+  };
+
+  return {
+    classicScore,
+    setClassicScore,
+    newScore,
+    setNewScore,
+    DXScore,
+    setDXScore,
+    scoreData,
+    setScoreData,
+    editScoreData,
+  };
 };
