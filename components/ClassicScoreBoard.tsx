a30d29ab35edc02f648fce554f706d8cdb70cd76
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-17 18:32:01 +0900

    feat: 旧筐体スコア表示用ボード作成

---
+++ components/ClassicScoreBoard.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { ClassicScoreColor, evaluationColor } from '../constants/color';
+
+export type ClassicScoreBoardProps = {
+  total: number;
+  max: number;
+  totalBreakBonus: number;
+  maxBreakBonus: number;
+};
+
+const StyledClassicScoreBoard = styled.div`
+  font-family: Noto Sans Noto Sans JP;
+  max-width: 600px;
+  border: 3px solid #3e9cf5;
+  border-radius: 5px;
+  border-collapse: separate;
+  border-spacing: 0;
+  padding: 10px 10px;
+  background-color: #606fb348;
+`;
+
+const BoardTitle = styled.div`
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  color: #606fb3;
+  letter-spacing: 2px;
+  text-shadow: white 2px 2px 0, white -2px -2px 0, /*右下、左上*/ white -2px 2px 0, white 2px -2px 0,
+    /*右上、左下*/ white 0px 2px 0, white 0 -2px 0, /*右、左*/ white -2px 0 0, white 2px 0 0;
+`;
+
+const Achievement = styled.div`
+  text-align: center;
+  font-size: 50px;
+  color: ${ClassicScoreColor['text']};
+  letter-spacing: 2px;
+  text-shadow: ${ClassicScoreColor['shadow']} 2px 2px 0, ${ClassicScoreColor['shadow']} -2px -2px 0,
+    /*右下、左上*/ ${ClassicScoreColor['shadow']} -2px 2px 0,
+    ${ClassicScoreColor['shadow']} 2px -2px 0,
+    /*右上、左下*/ ${ClassicScoreColor['shadow']} 0px 2px 0, ${ClassicScoreColor['shadow']} 0 -2px 0,
+    /*右、左*/ ${ClassicScoreColor['shadow']} -2px 0 0, ${ClassicScoreColor['shadow']} 2px 0 0;
+`;
+
+const AchievementPercent = styled.span`
+  margin-left: 3px;
+  font-size: 60%;
+`;
+
+const ScoreDetail = styled.div`
+  margin-top: 10px;
+  font-size: 20px;
+`;
+
+const MaxScore = styled.div``;
+
+const TotalScore = styled.div``;
+
+const BreakBonus = styled.div``;
+
+export const ClassicScoreBoard = ({
+  total,
+  max,
+  totalBreakBonus,
+  maxBreakBonus,
+}: ClassicScoreBoardProps) => {
+  return (
+    <StyledClassicScoreBoard>
+      <BoardTitle>旧筐体 達成率</BoardTitle>
+      <Achievement>
+        {Math.ceil((total / max) * 100 * Math.pow(10, 2)) / Math.pow(10, 2)}
+        <AchievementPercent>%</AchievementPercent>
+      </Achievement>
+      <ScoreDetail>
+        <MaxScore>Maxスコア: {max + maxBreakBonus}</MaxScore>
+        <TotalScore>Totalスコア: {total}</TotalScore>
+        <BreakBonus>
+          Breakボーナス: {totalBreakBonus} / {maxBreakBonus}
+        </BreakBonus>
+      </ScoreDetail>
+    </StyledClassicScoreBoard>
+  );
+};
